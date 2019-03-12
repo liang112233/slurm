f61111d0b38756f9fb5e3ebb2caac1c6952c4a7d
Tim Wickberg <tim@schedmd.com>
2019-03-12 01:40:59 -0600

    Convert --dependency option parsing to new form.

--- src/srun/libsrun/opt.c
+++ src/srun/libsrun/opt.c
@@ -141,7 +141,6 @@ struct option long_options[] = {
 	{"extra-node-info",  required_argument, 0, 'B'},
 	{"cpus-per-task",    required_argument, 0, 'c'},
 	{"cluster-constraint",required_argument,0, LONG_OPT_CLUSTER_CONSTRAINT},
-	{"dependency",       required_argument, 0, 'd'},
 	{"chdir",            required_argument, 0, 'D'},
 	{"error",            required_argument, 0, 'e'},
 	{"preserve-env",     no_argument,       0, 'E'},
@@ -614,7 +613,6 @@ static void _opt_default(void)
 		sropt.cwd_set		= false;
 		sropt.debugger_test	= false;
 		opt.delay_boot		= NO_VAL;
-		xfree(opt.dependency);
 		sropt.disable_status	= false;
 		opt.distribution	= SLURM_DIST_UNKNOWN;
 		opt.egid		= (gid_t) -1;
@@ -1272,10 +1270,6 @@ static void _set_options(const int argc, char **argv)
 			}
 			opt.cpus_set = true;
 			opt.cpus_per_task = tmp_int;
-			break;
-		case (int)'d':
-			xfree(opt.dependency);
-			opt.dependency = xstrdup(optarg);
 			break;
 		case (int)'D':
 			if (!optarg)
